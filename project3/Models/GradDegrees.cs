6ac0dca42b2ce8a24193ea0498e145b8b578105a
Saquib Ahmed <82791925+skib-khan@users.noreply.github.com>
2021-12-10 15:39:19 -0500

    Add files via upload

---
+++ project3/Models/GradDegrees.cs
@@ -0,0 +1,16 @@
+﻿namespace Project3_Base_Code.Models
+{
+    class GradDegrees
+    {
+        public List<Graduate> graduate { get; set; }
+    }
+
+    public class Graduate
+    {
+        public string degreeName { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public List<string> concentrations { get; set; }
+        public List<string> availableCertificates { get; set; }
+    }
+}
---
+++ project3/Models/UnderDegrees.cs
@@ -0,0 +1,17 @@
+﻿namespace Project3_Base_Code.Models
+{
+    class UnderDegrees
+    {
+        public List<Undergraduate> undergraduate { get; set; }
+       
+    }
+
+    public class Undergraduate
+    {
+        public string degreeName { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public List<string> concentrations { get; set; }
+    }
+
+}
