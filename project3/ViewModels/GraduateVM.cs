433a460decd66534e0fa2029c14ea287fcda6de8
Saquib Ahmed <82791925+skib-khan@users.noreply.github.com>
2021-12-10 15:44:12 -0500

    Add files via upload

---
+++ project3/ViewModels/GraduateVM.cs
@@ -0,0 +1,10 @@
+﻿using Project3_Base_Code.Models;
+
+namespace Project3_Base_Code.ViewModels
+{
+    public class GraduateVM
+    {
+        public List<GradDegrees> GradDegrees { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ project3/ViewModels/UndergraduateVM.cs
@@ -0,0 +1,10 @@
+﻿using Project3_Base_Code.Models;
+
+namespace Project3_Base_Code.ViewModels
+{
+    public class UndergraduateVM
+    {
+        public List<UnderDegrees> UnderDegrees { get; set; }
+        public string description { get; set; }
+    }
+}
