04e6a79d7f74114647f41d7413e22c9f42f0caf4
Saquib Ahmed <82791925+skib-khan@users.noreply.github.com>
2021-12-10 16:08:38 -0500

    Add files via upload

---
+++ project3/ViewModels/EmploymentVM.cs
@@ -0,0 +1,14 @@
+﻿using Project3_Base_Code.Models;
+
+namespace Project3_Base_Code.ViewModels
+{
+    public class EmploymentVM
+    {
+        public List<string> employerNames { get; set; }
+        public List<string> DegreeStatistics { get; set; }
+        public List<CoopInformation> coopInformation { get; set; }
+        public List<EmployerInformation> city { get; set; }
+        public List<EmployerInformation> startDate { get; set; }
+
+    }
+}
