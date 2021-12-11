11ea3529d7f6a9e4d3ca1989348d9d6d1de6761c
Saquib Ahmed <82791925+skib-khan@users.noreply.github.com>
2021-12-10 22:46:36 -0500

    Update Degrees.cs

--- project3/Models/Degrees.cs
+++ project3/Models/Degrees.cs
@@ -3,21 +3,7 @@
 
     class Degrees
     {
-        public List<Undergraduate> undergraduate { get; set; }
-        public List<Graduate> graduate { get; set; }
-    }
-
-
-    public class Undergraduate
-    {
-        public string degreeName { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        public List<string> concentrations { get; set; }
-    }
-
-    public class Graduate
-    {
+   
         public string degreeName { get; set; }
         public string title { get; set; }
         public string description { get; set; }
