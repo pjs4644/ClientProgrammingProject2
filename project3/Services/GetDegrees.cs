4d8a4c7a5555ec8333d5dc344729a4be873ee187
Peter Schwarzkopf <pjs4644@rit.edu>
2021-12-10 22:50:54 -0500

    get degrees and get minors

--- project3/Services/GetDegrees.cs
+++ project3/Services/GetDegrees.cs
@@ -2,5 +2,50 @@
 {
     public class GetDegrees : IGetDegrees
     {
+         public async Task<List<Degrees>> GetAllDegrees()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://www.ist.rit.edu/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("api/people/degrees", HttpCompletionOption.ResponseHeadersRead);
+                    response.EnsureSuccessStatusCode();
+                    var data = await response.Content.ReadAsStringAsync();
+
+                    var rtnResults = JsonSerializer.Deserialize<Dictionary<string, List<Faculty>>>(data);
+
+                    List<Degrees> degreeList = new List<Degrees>();
+
+                    foreach (KeyValuePair<string, List<Degrees>> kvp in rtnResults)
+                    {
+                        foreach (var item in kvp.Value)
+                        {
+                            degreeList.Add(item);
+                        }
+                    }
+
+                    return degreeList;
+                }
+
+                catch (HttpRequestException hre)
+                {
+                    var msg = hre.Message;
+                    List<Degrees> degreeList = new List<Degrees>();
+                    return degreeList;
+                    //return "HttpRequestException";
+                }
+                catch (Exception ex)
+                {
+                    var msg = ex.Message;
+                    List<Degrees> degreeList = new List<Degrees>();
+                    return degreeList;
+                    //return "Exception"; ;
+                }
+            }
+        }
     }
 }
--- project3/Services/GetMinors.cs
+++ project3/Services/GetMinors.cs
@@ -2,5 +2,50 @@
 {
     public class GetMinors : IGetMinors
     {
+        public async Task<List<Minor>> GetAllMinor()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://www.ist.rit.edu/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("api/people/minors", HttpCompletionOption.ResponseHeadersRead);
+                    response.EnsureSuccessStatusCode();
+                    var data = await response.Content.ReadAsStringAsync();
+
+                    var rtnResults = JsonSerializer.Deserialize<Dictionary<string, List<AboutUs>>>(data);
+
+                    List<Minor> minorList = new List<Minor>();
+
+                    foreach (KeyValuePair<string, List<Minor>> kvp in rtnResults)
+                    {
+                        foreach (var item in kvp.Value)
+                        {
+                            minorList.Add(item);
+                        }
+                    }
+
+                    return minorList;
+                }
+                catch (HttpRequestException hre)
+                {
+                    var msg = hre.Message;
+                    List<Minor> minorList = new List<Minor>();
+                    return minorList;
+                    //return "HttpRequestException";
+                }
+                catch (Exception ex)
+                {
+                    var msg = ex.Message;
+                    List<Minor> minorList = new List<Minor>();
+                    return minorList;
+                    //return "Exception"; ;
+                }
+            }
+
+        }
     }
 }
